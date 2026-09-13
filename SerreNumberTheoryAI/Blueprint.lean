dfa84d483bdcdbab564bf5661576d4b19953be0d
Are <235915091+are4c4@users.noreply.github.com>
2026-09-14 07:01:48 +0900

    Integrate Proposition 5 Blueprint into aggregator

--- SerreNumberTheoryAI/Blueprint.lean
+++ SerreNumberTheoryAI/Blueprint.lean
@@ -12,6 +12,7 @@ import SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyNontrivialZero
 import SerreNumberTheoryAI.Blueprint.Chapter01.QuadraticElements
 import SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyQuadraticForm
 import SerreNumberTheoryAI.Blueprint.Chapter02.PadicIntegers
+import SerreNumberTheoryAI.Blueprint.Chapter02.RootExistence
 
 open Verso.Genre
 open Verso.Genre.Manual
@@ -40,6 +41,8 @@ open Informal
 {include 0 SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyQuadraticForm}
 
 {include 0 SerreNumberTheoryAI.Blueprint.Chapter02.PadicIntegers}
+
+{include 0 SerreNumberTheoryAI.Blueprint.Chapter02.RootExistence}
 
 # 定理の依存関係
 
