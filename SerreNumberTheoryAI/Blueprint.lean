ee8f3a0b89b981f2069b642882fc0f795f1fe31f
Are <235915091+are4c4@users.noreply.github.com>
2026-09-14 04:24:19 +0900

    Include Chevalley quadratic-form Blueprint

--- SerreNumberTheoryAI/Blueprint.lean
+++ SerreNumberTheoryAI/Blueprint.lean
@@ -9,6 +9,7 @@ import SerreNumberTheoryAI.Blueprint.Chapter01.MultiplicativeGroup
 import SerreNumberTheoryAI.Blueprint.Chapter01.PowerSums
 import SerreNumberTheoryAI.Blueprint.Chapter01.Chevalley
 import SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyNontrivialZero
+import SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyQuadraticForm
 
 open Verso.Genre
 open Verso.Genre.Manual
@@ -31,6 +32,8 @@ open Informal
 {include 0 SerreNumberTheoryAI.Blueprint.Chapter01.Chevalley}
 
 {include 0 SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyNontrivialZero}
+
+{include 0 SerreNumberTheoryAI.Blueprint.Chapter01.ChevalleyQuadraticForm}
 
 # 定理の依存関係
 
