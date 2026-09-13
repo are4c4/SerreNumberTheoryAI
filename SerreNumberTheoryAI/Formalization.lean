793a413ef0f8278d656eb90eff54cabed956ca06
Are <235915091+are4c4@users.noreply.github.com>
2026-09-14 04:30:51 +0900

    Integrate p-adic integers into formalization root

--- SerreNumberTheoryAI/Formalization.lean
+++ SerreNumberTheoryAI/Formalization.lean
@@ -3,6 +3,8 @@ import SerreNumberTheoryAI.Formalization.Chapter01.MultiplicativeGroup
 import SerreNumberTheoryAI.Formalization.Chapter01.PowerSums
 import SerreNumberTheoryAI.Formalization.Chapter01.Chevalley
 import SerreNumberTheoryAI.Formalization.Chapter01.ChevalleyNontrivialZero
+import SerreNumberTheoryAI.Formalization.Chapter01.QuadraticElements
+import SerreNumberTheoryAI.Formalization.Chapter02.PadicIntegers
 
 /-!
 # Formalization root
